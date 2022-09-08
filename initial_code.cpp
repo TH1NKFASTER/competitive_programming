610c8e070477044ae0d4e87d04bc89b520920b85
tfr <dolgov.me@phystech.edu>
2022-09-08 14:05:01 +0300

    Tests, simple includes, simple type aliases

---
+++ initial_code.cpp
@@ -0,0 +1,15 @@
+//
+// Created by tfr on 08.09.2022.
+//
+
+#include <iostream>
+
+using ll = long long;
+using ull = unsigned long long;
+
+int main() {
+    ull tests_in_task = 1;
+    while (tests_in_task--) {
+
+    }
+}
