7733f37f1c81907f659b13dc42a5a31bbb71372d
tfr <dolgov.me@phystech.edu>
2022-09-08 14:59:34 +0300

    Added accumulate-like function from python, added reading and writing for vectors

--- Initial code/initial_code.cpp
+++ Initial code/initial_code.cpp
@@ -1,11 +1,39 @@
 //
 // Created by tfr on 08.09.2022.
 //
-
+#include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <vector>
+#include <functional>
 
 using ll = long long;
 using ull = unsigned long long;
+
+template<class T, class BinaryOperation=std::function<T(T, T)>>
+std::vector<T> PrefixOp(const std::vector<T> &v, BinaryOperation operation = std::plus<T>(), const T &neutral = T()) {
+    std::vector<T> result(v.size() + 1);
+    result[0] = neutral;
+    for (unsigned i = 0; i < v.size(); ++i) {
+        result[i + 1] = (operation(result[i], v[i]));
+    }
+    return result;
+}
+
+template<class T>
+void printVector(const std::vector<T> &v) {
+    for (auto &t: v) {
+        std::cout << t << " ";
+    }
+    std::cout << "\n";
+}
+
+template<class T>
+void readVector(std::vector<T> &v) {
+    for (auto &t: v) {
+        std::cin >> t;
+    }
+}
 
 int main() {
     ull tests_in_task = 1;
