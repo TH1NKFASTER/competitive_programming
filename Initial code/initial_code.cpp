018f0e9ae21cd7034259287e77ef73a40556c873
tfr <dolgov.me@phystech.edu>
2022-09-08 15:43:46 +0300

    added Combinatorics class, IntMod class, fast i/o, class to use prefix operation array

--- Initial code/initial_code.cpp
+++ Initial code/initial_code.cpp
@@ -21,6 +21,12 @@ std::vector<T> PrefixOp(const std::vector<T> &v, BinaryOperation operation = std
 }
 
 template<class T>
+class OperationOnSegmentUsingPrefixOp {
+
+
+};
+
+template<class T>
 void printVector(const std::vector<T> &v) {
     for (auto &t: v) {
         std::cout << t << " ";
@@ -35,7 +41,71 @@ void readVector(std::vector<T> &v) {
     }
 }
 
+class IntMod {
+public:
+    IntMod() {
+        number = 0;
+    }
+
+    IntMod(ull x) {
+        number = x % modular;
+    }
+
+
+    ull number;
+    const ull modular = 1000000007;
+};
+
+template<class T>
+class Combinatorics {
+public:
+    Combinatorics() {
+        factorials.resize(max_n + 1);
+        factorials[0] = 1;
+        for (unsigned long long i = 1; i <= max_n; ++i) {
+            factorials[i] = (factorials[i - 1] * i) % modular;
+        }
+        inverse_factorials.resize(max_n + 1);
+        for (unsigned long long i = 0; i <= max_n; ++i) {
+            inverse_factorials[i] = inverse_modular(factorials[i]);
+        }
+    }
+
+    T fast_power(T base, T pow) {
+        if (pow == 0) {
+            return 1;
+        }
+        T half = fast_power(base, pow / 2);
+        if (pow % 2) {
+            return half * half * base;
+        } else {
+            return half * half;
+        }
+    }
+
+    T inverse_modular(T num) {
+        return fast_power(num, modular - 2);
+    }
+
+    T Cnk(T n, T k) {
+        return factorials[n] * inverse_factorials[k] * inverse_factorials[n - k];
+    }
+
+    T Ank(T n, T k) {
+        return factorials[n] * inverse_factorials[n - k];
+    }
+
+private:
+    unsigned long long max_n = 1e6;
+    std::vector<T> factorials;
+    std::vector<T> inverse_factorials;
+    unsigned long long modular = 1000000007;
+};
+
 int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie();
+    std::cout.tie();
     ull tests_in_task = 1;
     while (tests_in_task--) {
 
